13a3adb270c4388a6bb0ca4848c1925d1b0186b4
Buithienbao <thienbaobuisn@gmail.com>
2021-05-31 17:26:49 +0200

    modified compute tool pose

--- pose_estimation_code/undistort_img.m
+++ pose_estimation_code/undistort_img.m
@@ -1,24 +1,24 @@
-DATA_PATH_IMAGE = [ '/home/bao/Downloads/trocar_estimation_adrien/mercuri/' ];
+DATA_PATH_IMAGE = [ '/home/bao/Downloads/trocar_estimation_adrien/finat/' ];
 
-indicesToProcess = [ 43 44 49 52 61 ];
+indicesToProcess = [ 66 88 90 101 121 ];
 
-imagePrefix = '%02d';
+imagePrefix = '%03d';
 
-fx = 9.5206967601396957e+02;
+fx = 1.9627645283697193e+03;
 
-fy = 9.5206967601396957e+02;
+fy = 1.9569201703481888e+03;
 
-cx = 8.8447392341747241e+02;
+cx = 1.0457889350948246e+03;
 
-cy = 5.5368748726315528e+02;
+cy = 4.7855060990037157e+02;
 
-k1 = -2.3184898303739043e-01;
+k1 = -2.5094556282958186e-01;
 
-k2 = 3.4051349187505525e-01;
+k2 = 4.7288007862790116e-01;
 
-k3 = -2.3860036358921605e-01;
+k3 = -4.4664893111535614e-01;
 
-K = [ fx 0 cx ; 0 fy cy ; 0 0 1 ]
+K = [ fx 0 cx ; 0 fy cy ; 0 0 1 ];
 
 
 
@@ -28,17 +28,17 @@ focal = [fx, fy];
 
 prip = [cx, cy];
 
-imsiz = [size(I1,2),size(I1,1)];
-
-radial = [k1,k2,k3];
-
-camIntrinsics = cameraIntrinsics(focal, prip,imsiz,'RadialDistortion', radial);
-
-cameraParams = cameraParameters('IntrinsicMatrix',camIntrinsics.IntrinsicMatrix,'RadialDistortion',camIntrinsics.RadialDistortion);
-
 for iFrameToProcess = indicesToProcess
-
+    
     I1 = imread([DATA_PATH_IMAGE 'image' sprintf([ imagePrefix '.png' ],iFrameToProcess)]);
+
+    imsiz = [size(I1,2),size(I1,1)];
+
+    radial = [k1,k2,k3];
+
+    camIntrinsics = cameraIntrinsics(focal, prip,imsiz,'RadialDistortion', radial);
+
+    cameraParams = cameraParameters('IntrinsicMatrix',camIntrinsics.IntrinsicMatrix,'RadialDistortion',camIntrinsics.RadialDistortion);
 
     % Undistort images:
 
